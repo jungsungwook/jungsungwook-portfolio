3182680f68aa1d4b4f44049b8d4e3732997f1195
jungsungwook <jswcyber@naver.com>
2023-10-24 11:25:44 +0900

    fix: header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,19 +11,9 @@ const HomeIndex = () => {
 
     return (
         <>
-            <Head>
-                <meta property="og:url" content="https://sungwook.net/" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image" content="https://avatars.githubusercontent.com/u/20926860?v=4" />
-                <meta property="og:description" content="공유기능 | 열심히 코드를 공부합시다." />
-                <title>정성욱의 포트폴리오 웹사이트</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-
-                <MainFrame
-                    page={0}
-                ></MainFrame>
-            
+            <MainFrame
+                page={0}
+            ></MainFrame>
         </>
     );
 };
