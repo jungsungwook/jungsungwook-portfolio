7959d707ffb6c35e4dfa15e08bb5f7d2bf9ec147
jungsungwook <jswcyber@naver.com>
2023-10-26 17:50:56 +0900

    feat: experience page

---
+++ src/pages/page/experiencePage.tsx
@@ -0,0 +1,320 @@
+const ExperiencePage = () => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+        }}>
+            <div className="profile-skill"
+                style={{
+                    display: "flex",
+                    alignItems: "flex-start",
+                    flexDirection: "column",
+                    // 맨 위로 붙이기
+                    marginTop: "0px",
+                    // 왼쪽으로 붙이기
+                    marginLeft: "20px",
+                    // 간격
+                    gap: "9px",
+                }}
+            >
+                <div className="profile-skill-title"
+                    style={{
+                        display: "flex",
+                        justifyContent: "flex-start",
+                    }}
+                >
+                    <span className="en"
+                        style={{
+                            fontSize: "20px",
+                            fontWeight: "bold",
+                        }}
+                    >
+                        Experience
+                    </span>
+                </div>
+                <hr style={{
+                    width: "95%",
+                    margin: "5px 0 0 0",
+                    border: "2px solid rgba(0,0,0,.15)",
+                }} />
+                <div className="profile-skill-content"
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "10px",
+                    }}
+                >
+                    <div className="profile-skill-content-item"
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "10px",
+                        }}
+                    >
+                        <span className="en"
+                            style={{
+                                fontSize: "15px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            2018
+                        </span>
+                        <div className="experience-content">
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                IOT 개발 담당 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 Wel-Tech사업부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                JAVA 전공멘토 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 소프트웨어응용학부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                Python 기초교양 멘토 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                학습리더 전공특강 강연 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                학습지원 서포터즈 활동 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                가상현실 학부 학술 동아리 CAVE 부회장 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 소프트웨어응용학부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                제1회 가상현실컨텐츠 개발전시회 기획 및 운영총괄 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 소프트웨어응용학부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                교외 학부 홍보 활동 (고등학교) <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 참인재교학팀</span>
+                            </span>
+                        </div>
+                    </div>
+                    <hr style={{
+                        width: "95%",
+                        margin: "5px 0 0 0",
+                        border: "0.1px solid rgba(0,0,0,.15)",
+                    }} />
+                    <div className="profile-skill-content-item"
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "10px",
+                        }}
+                    >
+                        <span className="en"
+                            style={{
+                                fontSize: "15px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            2019
+                        </span>
+                        <div className="experience-content">
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                인공지능 학부 학술 동아리 CEL 회장 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 소프트웨어응용학부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                웹프로그래밍 전공멘토 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 소프트웨어응용학부</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                Python 기초교양 멘토 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                학습리더 전공특강 강연 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                학습지원 서포터즈 활동 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 교수학습지원센터</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                신입생 SW멘토링 멘토 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 참인재교학팀</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                교외 학부 홍보 활동 (고등학교) <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 강남대학교 참인재교학팀</span>
+                            </span>
+                        </div>
+                    </div>
+                    <hr style={{
+                        width: "95%",
+                        margin: "5px 0 0 0",
+                        border: "0.1px solid rgba(0,0,0,.15)",
+                    }} />
+                    <div className="profile-skill-content-item"
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "10px",
+                        }}
+                    >
+                        <span className="en"
+                            style={{
+                                fontSize: "15px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            2020
+                        </span>
+                        <div className="experience-content">
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                품질기획 부서 인턴 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 라온시큐어 (3개월)</span>
+                            </span>
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                프로그래밍 전임 강사 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| 메이킷코드랩 송도센터 (6개월)</span>
+                            </span>
+                        </div>
+                    </div>
+                    <hr style={{
+                        width: "95%",
+                        margin: "5px 0 0 0",
+                        border: "0.1px solid rgba(0,0,0,.15)",
+                    }} />
+                    <div className="profile-skill-content-item"
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "10px",
+                        }}
+                    >
+                        <span className="en"
+                            style={{
+                                fontSize: "15px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            2021
+                        </span>
+                        <div className="experience-content">
+                            <span className="en"
+                                style={{
+                                    fontSize: "15px",
+                                }}
+                            >
+                                프로그래밍 전임 강사 <span style={{
+                                    fontSize: "14px",
+                                    color: "#999",
+                                }}>| C³ coding(씨큐브코딩) 송도국제센터 (6개월)</span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExperiencePage;
