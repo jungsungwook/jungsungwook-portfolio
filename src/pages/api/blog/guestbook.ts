171115ea6e456deea99f02297e9194a6d1ca5054
jungsungwook <jswcyber@naver.com>
2024-01-17 10:30:47 +0900

    fix: ip address

--- src/pages/api/blog/guestbook.ts
+++ src/pages/api/blog/guestbook.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import DbHandler from '@/database/dbHandler';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const ip = req.connection.remoteAddress;
+    const ip = req.headers['x-forwarded-for']
 
     res.status(200).json({
         "statusCode": 200,
