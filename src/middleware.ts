6f04a13c101d56a09e2ce4dc6dc1941a3d81f5c4
jungsungwook <jswcyber@naver.com>
2024-01-31 14:03:54 +0900

    feat: middlware checking user

---
+++ src/middleware.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import axios, { Method } from 'axios';
+import { getApiUrl } from './utils/getApiUrl';
+
+export async function middleware(request: NextRequest) {
+    try {
+        let token: string = request.cookies.get('token')?.value as string;
+        if (!token) {
+            token = request.headers.get('authorization') as string;
+            if (token) token = token.split(" ")[1];
+        }
+        if (!token) {
+            token = request.headers.get('authorization') as string;
+            return request.nextUrl.pathname.startsWith('/api') ? Response.json({ statusCode: 401, content: 'Unauthorized' }, { status: 401 })
+                : NextResponse.redirect(new URL('/auth/signin', request.url));
+        }
+
+        try {
+            const response = await fetch(getApiUrl('/auth/role'), {
+                method: 'get' as Method,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+
+            if (data.statusCode == 200 || data.statusCode == '200') {
+                const role = data.content;
+                if (role < 2) {
+                    return request.nextUrl.pathname.startsWith('/api') ? Response.json({ statusCode: 401, content: 'Unauthorized' }, { status: 401 })
+                        : NextResponse.redirect(new URL('/auth/signin', request.url));
+                }
+            } else {
+                return request.nextUrl.pathname.startsWith('/api') ? Response.json({ statusCode: 401, content: 'Unauthorized' }, { status: 401 })
+                    : NextResponse.redirect(new URL('/auth/signin', request.url));
+            }
+        } catch (e) {
+            return request.nextUrl.pathname.startsWith('/api') ? Response.json({ statusCode: 401, content: 'Unauthorized' }, { status: 401 })
+                : NextResponse.redirect(new URL('/auth/signin', request.url));
+        }
+        return NextResponse.next();
+    }
+    catch (e) {
+        return Response.json({ statusCode: 500, content: 'Exception!' }, { status: 500 })
+    }
+}
+
+export const config = {
+    matcher: ['/admin/:path*']
+};
