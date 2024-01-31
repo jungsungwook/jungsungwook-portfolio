62fb53f37468e27d10a03388f0e9bae7206cd7af
jungsungwook <jswcyber@naver.com>
2024-01-31 14:00:11 +0900

    feat: admin page- edit blog posting

---
+++ src/pages/admin/blog/new.tsx
@@ -0,0 +1,19 @@
+import EditFrame from "@/components/edit-frame";
+
+const AdminBlogNew = () => {
+    return (
+        <>
+            <div
+                style={{
+                    paddingTop: '10%'
+                }}
+            >
+                <EditFrame
+                    type="new"
+                ></EditFrame>
+            </div>
+        </>
+    )
+}
+
+export default AdminBlogNew;
